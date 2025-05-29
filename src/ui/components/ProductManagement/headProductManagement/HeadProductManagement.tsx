d134018757f1671bfb17ed38ef67eaba15b827db
kgnot <hricaurtem@proton.me>
2025-05-29 11:01:15 -0500

    Product section: Add to add product button functionality corresponding to its name

--- src/ui/components/ProductManagement/headProductManagement/HeadProductManagement.tsx
+++ src/ui/components/ProductManagement/headProductManagement/HeadProductManagement.tsx
@@ -1,22 +1,41 @@
 import './HeadProductManagement.css'
-import {ButtonType1} from "../../utils/buttons/ButtonType1.tsx";
-import {ButtonType2} from "../../utils/buttons/ButtonType2.tsx";
-import {useRef} from "react";
+import {ButtonType1,ButtonType2} from "../../utils/buttons";
+import {useRef, useState} from "react";
 import {SearchInput} from "../../utils/searchInput/SearchInput.tsx";
 import {/*fetchItemsByLetterAPI,*/ fetchItemsByWordAPI} from "../../../../function";
 import {Product} from "../../../../entity";
+import {BlackModal} from '../../utils/modal/BalckModal.tsx';
+import {AddProductModal} from "./modal/AddProductModal.tsx";
 
 
 interface HeadProductManagementProps {
     className: string;
     searchProduct: (letter: string) => void;
+    addProductMethod: (Product: Product) => void;
 }
 
-export const HeadProductManagement = ({className, searchProduct}: HeadProductManagementProps) => {
+export const HeadProductManagement = (
+    {
+        className,
+        searchProduct,
+        addProductMethod,
+    }: HeadProductManagementProps) => {
+    // apartado del modal:
+    const [showModal, setShowModal] = useState(false);
+
+    const closePreview = () => {
+        setShowModal(false);
+    };
+
     const inputSearchProduct = useRef<HTMLInputElement>(null);
 
     const handleProductSelect = (product: Product) => {
         searchProduct(product.description);
+    };
+
+    const handleAddProduct = (product: Product) => {
+        addProductMethod(product);
+        setShowModal(false); // Cerrar el modal después de agregar
     };
 
     return (
@@ -39,7 +58,13 @@ export const HeadProductManagement = ({className, searchProduct}: HeadProductMan
                 <ButtonType2 parentMethod={() => searchProduct(inputSearchProduct.current?.value || '')}>
                     Buscar Producto
                 </ButtonType2>
-                <ButtonType1>Añadir Producto</ButtonType1>
+                <ButtonType1 parentMethod={() => setShowModal(true)}>
+                    Añadir Producto
+                </ButtonType1>
+                {/*    Modal para agregar producto */}
+                <BlackModal visible={showModal} onClose={closePreview}>
+                    <AddProductModal parentMethod={handleAddProduct} />
+                </BlackModal>
             </div>
         </section>
     );
