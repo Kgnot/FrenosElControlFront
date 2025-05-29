4e077f38aec6567f4ed7f54e3a7d75683a7e0c18
kgnot <hricaurtem@proton.me>
2025-05-29 16:26:15 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts o vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2020',
-    minify: true,
-    sourcemap: false,
-  },
-});
+  base: './',
+})
